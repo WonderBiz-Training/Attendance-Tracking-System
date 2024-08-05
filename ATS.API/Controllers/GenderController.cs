a7e5cb967229a7007418059ec16b88765b6b5afe
Anirudh Gupta <anirudh.gupta@wonderbiz.in>
2024-08-05 17:40:19 +0530

    Created Gender APIs

---
+++ ATS.API/Controllers/GenderController.cs
@@ -0,0 +1,95 @@
+﻿using ATS.DTO;
+using ATS.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ATS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenderController : ControllerBase
+    {
+        private readonly IGenderServices _genderServices;
+
+        public GenderController(IGenderServices genderServices)
+        {
+            _genderServices = genderServices;
+        }
+
+        // GET: api/<GenderController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<GetGenderDto>>> Get()
+        {
+            try
+            {
+                var res = await _genderServices.GetAllGendersAsync();
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        // GET api/<GenderController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<GetGenderDto>> Get(int id)
+        {
+            try
+            {
+                var res = await _genderServices.GetGenderAsync(id);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        // POST api/<GenderController>
+        [HttpPost]
+        public async Task<ActionResult<GetGenderDto>> Post([FromBody] CreateGenderDto genderDto)
+        {
+            try
+            {
+                var res = await _genderServices.CreateGenderAsync(genderDto);
+                return CreatedAtAction(nameof(Get), new { id = res.Id }, res);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        // PUT api/<GenderController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult<GetGenderDto>> Put(int id, [FromBody] UpdateGenderDto genderDto)
+        {
+            try
+            {
+                var res = await _genderServices.UpdateGenderAsync(id, genderDto);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        // DELETE api/<GenderController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<bool>> Delete(int id)
+        {
+            try
+            {
+                var res = await _genderServices.DeleteGenderAsync(id);
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+    }
+}
--- ATS.IServices/IGenderServices.cs
+++ ATS.IServices/IGenderServices.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ATS.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,14 @@ namespace ATS.IServices
 {
     public interface IGenderServices
     {
+        Task<IEnumerable<GetGenderDto>> GetAllGendersAsync();
+
+        Task<GetGenderDto> GetGenderAsync(long id);
+
+        Task<GetGenderDto> CreateGenderAsync(CreateGenderDto genderDto);
+
+        Task<GetGenderDto> UpdateGenderAsync(long id, UpdateGenderDto genderDto);
+
+        Task<bool> DeleteGenderAsync(long id);
     }
 }
