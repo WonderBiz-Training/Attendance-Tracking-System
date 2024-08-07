968ec0a81005f7f80b93312062b6a9fe503ef19d
Anirudh Gupta <anirudh.gupta@wonderbiz.in>
2024-08-07 15:12:22 +0530

    one to one

---
+++ ATS.API/Migrations/20240807094206_AddedUserDetailsIdToUserTable1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ATS.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedUserDetailsIdToUserTable1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
