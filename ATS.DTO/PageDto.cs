d4940c29e44615237e25b40f3b759f59598c8eae
Nimesh Jethva <nimesh.jethva@wonderbiz.in>
2024-08-27 10:17:47 +0530

    Added Page APIs

---
+++ ATS.DTO/PageDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATS.DTO
+{
+    public class PageDto
+    {
+
+    }
+
+    public record CreatePageDto(
+        [MaxLength(100)] string PageCode,
+        [Required(ErrorMessage = "Page Title is Required"), MaxLength(100)] string PageTitle,
+        long CreatedBy
+    );
+
+    public record UpdatePageDto(
+        string PageCode,
+        string PageTitle,
+        long UpdatedBy
+    );
+
+    public record GetPageDto(
+        long Id,
+        string PageCode,
+        string PageTitle
+    );
+}
